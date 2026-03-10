3a79c61e3433361e047adb27cb3635c68ab36b4e
sudheeruppuluri <sudheer@sjmedialabs.com>
2026-03-10 11:01:44 +0530

    hot fix

--- components/ui/input.tsx
+++ components/ui/input.tsx
@@ -42,7 +42,7 @@ function Input({ className, min, type, onKeyDown, onWheel, ...props }: React.Com
         if (isNumber && (e.key === "ArrowUp" || e.key === "ArrowDown")) {
           e.preventDefault() // prevent arrow key change
         }
-          if (["e", "E", "+", "-"].includes(e.key)) {
+          if (["+", "-"].includes(e.key)) {
       e.preventDefault()
     }
         onKeyDown?.(e)
